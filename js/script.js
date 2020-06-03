c296b9c866c1b829499450d9a77141a20cb227c0
93karolina <62308919+93karolina@users.noreply.github.com>
2020-06-03 23:08:02 +0200

    dodano pomysl na warunek z font awesome

--- js/script.js
+++ js/script.js
@@ -9,6 +9,8 @@ let buy = $(".buy");
 let sell = $(".sell");
 let btn = $(".btn");
 
+let beforeBuy = buy.text();
+let beforeSell = sell.text();
 
 $.getJSON(
     "https://blockchain.info/pl/ticker",
@@ -24,15 +26,31 @@ $.getJSON(
                 currency: key,
                 buy: obj[key].buy,
                 sell: obj[key].sell
-            }
+            }      
 
-
+           
             btn.click( function() {
                 buy.text(objCurrency.buy),
                 sell.text(objCurrency.sell);
 
                 console.log(objCurrency.currency);
-                  
+
+                let afterBuy = obj[key].buy;
+        
+                if (beforeBuy < afterBuy) {
+                    console.log("zwiekszylo sie");
+                    $(".arrow-buy").html(`<i class="fas fa-long-arrow-alt-up green" ></i>`)
+                    
+                } else if ( beforeBuy === afterBuy) {
+                    console.log("taki sam");
+                    $(".arrow-buy").html(`<i class=""fas fa-minus blue" ></i>`)
+
+                } else {
+                    console.log("zmniejszylo");
+                    $(".arrow-buy").html(`<i class="fas fa-long-arrow-alt-down red"></i>`)
+                }
+
+                
          })
      });    
  })
